ea514d97caf3cde96b7c01bd980e133a6b11318f
Jacob Strubelt <jacob.strubelt@outlook.com>
2025-05-15 20:39:00 -0500

    Update insider.py

--- insider.py
+++ insider.py
@@ -1,18 +1,1 @@
-import socket
-
-if __name__ == "__main__":
-    HOST = "0.0.0.0"
-    PORT = 1069
-
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-        s.bind((HOST,PORT))
-        s.listen()
-        print(f"Waiting to let myself in through port {PORT}...")
-        conn, addr = s.accept()
-        with conn:
-            print(f"Let myself in through ip {addr}")
-            while True:
-                data = conn.recv(1024)
-                if not data:
-                    break
-                conn.sendall(data)
+print("hello world!")
